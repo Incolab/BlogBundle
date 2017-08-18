299550b578720b95b0ac214190b918d0afea3352
David Salbei <david@chalvia.fr>
2017-08-18 17:27:08 +0200

    edit controller tu use comment service and IncolabCoreBundle Pagination

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,19 +4,21 @@ namespace Incolab\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Incolab\BlogBundle\Service\CommentService;
 use Incolab\BlogBundle\Entity\Comment;
 use Incolab\BlogBundle\Form\Type\CommentType;
-use Incolab\BlogBundle\IncolabBlogEvents;
-use Incolab\BlogBundle\Event\CommentEvent;
 
-class DefaultController extends Controller {
+class DefaultController extends Controller
+{
 
-    public function indexAction(Request $request) {
+    public function indexAction(Request $request)
+    {
         //return $this->forward('IncolabBlogBundle:Default:showPage', ['page' => 1]);
         return $this->showPageAction(1);
     }
 
-    public function showPageAction($page) {
+    public function showPageAction($page)
+    {
         if ($page < 1) {
             throw $this->createNotFoundException('Une page ne peux pas être inférieur à 1');
         }
@@ -31,31 +33,15 @@ class DefaultController extends Controller {
             throw $this->createNotFoundException('Cette page n\'existe pas');
         }
 
-        $pageParam = [
-            'prev' => false,
-            'next' => false,
-            'prevPage' => 0,
-            'nextPage' => 0
-        ];
-
-        $debut = $page;
         $numNews = $newsRepository->getTotalPublishedNumber();
-        if (($debut) > 1) {
-            $pageParam['prevPage'] = $debut - 1;
-            $pageParam['prev'] = true;
-        }
-
-        $nextPage = $debut + 1;
-
-        if (($page * $limit) < $numNews) {
-            $pageParam['nextPage'] = $nextPage;
-            $pageParam['next'] = true;
-        }
+        $pageParam = $this->get("incolab_core.pagination")
+                ->getSimplePagination($numNews, $page, $limit);
 
         return $this->render('IncolabBlogBundle:Default:index.html.twig', ['news' => $news, 'pageParam' => $pageParam]);
     }
 
-    public function showNewsAction($slug) {
+    public function showNewsAction($slug)
+    {
         $news = $this->get('db')
                         ->getRepository("IncolabBlogBundle:News")->findOneAndCommBySlug($slug);
 
@@ -65,47 +51,35 @@ class DefaultController extends Controller {
 
         $formComment = $this->createForm(CommentType::class, new Comment());
 
-        return $this->render('IncolabBlogBundle:News:show.html.twig', ['news' => $news, 'formComment' => $formComment->createView()]);
+        return $this->render('IncolabBlogBundle:News:show.html.twig', ['news' => $news, 'form' => $formComment->createView()]);
     }
 
-    public function createCommentAction($slug, Request $request) {
+    public function createCommentAction($slug)
+    {
         if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
             throw $this->createAccessDeniedException();
         }
-        $newsRepository = $this->get("db")->getRepository("IncolabBlogBundle:News");
-        $news = $newsRepository->findOneAndCommBySlug($slug);
 
-        if ($news === NULL) {
-            throw $this->createNotFoundException('News not found.');
+        $commentService = $this->get("blog.comment");
+
+        $res = $commentService->addComment($slug, $this->getUser());
+        $renderArgs = $commentService->getRenderArgs();
+
+        switch ($res) {
+            case CommentService::NEWS_NOT_FOUND:
+                throw $this->createNotFoundException('News not found.');
+
+            case CommentService::COMMENT_ADDED:
+                $this->addFlash('success', 'Comment added');
+                return $this->redirectToRoute('blog_news_show', ['slug' => $renderArgs["slug"]]);
+
+            case CommentService::FORM_INVALID:
+                return $this->render('IncolabBlogBundle:News:show.html.twig', $renderArgs);
+
+            case CommentService::EVT_INVALID:
+                $this->addFlash('error', $renderArgs["error"]);
+                return $this->render('IncolabBlogBundle:News:show.html.twig', $renderArgs);
         }
-
-        $comment = new Comment();
-
-        $formComment = $this->createForm(CommentType::class, $comment);
-        $formComment->handleRequest($request);
-
-        if ($formComment->isSubmitted() && $formComment->isValid()) {
-            $comment->setAuthor($this->getUser());
-            $comment->setNews($news);
-            $comment->setCreatedAt(new \DateTime());
-
-            $event = new CommentEvent($comment);
-            $this->get("event_dispatcher")->dispatch(IncolabBlogEvents::ON_COMMENT_POST, $event);
-
-            if (!$event->isValid()) {
-                $this->addFlash('error', $event->getMessageStatus());
-                return $this->render('IncolabBlogBundle:News:show.html.twig', ['comment' => $comment, 'news' => $news, 'formComment' => $formComment->createView()]);
-            }
-
-            $commentRepository = $this->get('db')->getRepository("IncolabBlogBundle:Comment");
-            $commentRepository->persist($comment);
-
-            $this->addFlash('success', 'Comment added');
-
-            return $this->redirectToRoute('blog_news_show', ['slug' => $news->getSlug()]);
-        }
-
-        return $this->render('IncolabBlogBundle:News:show.html.twig', ['comment' => $comment, 'news' => $news, 'formComment' => $formComment->createView()]);
     }
 
 }
