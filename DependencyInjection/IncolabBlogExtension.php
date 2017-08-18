0b4db13baf9874f4625f0a72bac52f76ed6a2b1c
David Salbei <david@chalvia.fr>
2017-08-18 17:19:48 +0200

    add service.yml file to dependency

--- DependencyInjection/IncolabBlogExtension.php
+++ DependencyInjection/IncolabBlogExtension.php
@@ -4,6 +4,8 @@ namespace Incolab\BlogBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
 
 use Incolab\BlogBundle\DependencyInjection\Configuration;
 
@@ -11,6 +13,9 @@ class IncolabBlogExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+        
         $configuration = new Configuration();
         
         $config = $this->processConfiguration($configuration, $configs);
